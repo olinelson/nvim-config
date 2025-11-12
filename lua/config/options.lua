1af9777d8a2b0df6857150272a06a44b9a9b8f98
Oli Nelson <olinelson@Olis-MacBook-Pro-Uscreen.local>
2025-11-12 16:43:21 +1100

    stop nvim from changing directories

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
-vim.opt.autochdir = false -- Prevent auto cd to file dir
+vim.g.root_spec = { "cwd" }
